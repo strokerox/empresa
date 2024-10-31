b5756497d76870a1d3e77d0b4be766e8b32136b3
strokerox <148830055+strokerox@users.noreply.github.com>
2024-10-31 07:39:35 -0400

    Create Gerente.java

---
+++ empresa/Salarios/Gerente.java
@@ -0,0 +1,9 @@
+// Ubicación: empresa/salarios/Gerente.java
+package empresa.salarios;
+
+public class Gerente extends Empleado {
+
+    public Gerente() {
+        super(350); // El salario base del gerente es 350$
+    }
+}
